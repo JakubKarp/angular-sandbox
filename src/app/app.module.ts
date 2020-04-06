ea594f36379df5e9ca66857d6edcee9f5d8d1781
j.karp <j.karp@createit.com>
2020-04-06 15:11:57 +0200

    first-com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderItemComponent } from './header-item/header-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  styles: [`
+    div {
+      background: red
+    }
+  `]
+})
+
+export class HeaderComponent {
+  title = 'header';
+  num = 1;
+  is = true;
+  disa = true;
+  paragraphText = 'Oh, I am paragraphText';
+
+  getNumber() {
+    return this.num;
+  }
+
+  onCreateParagraphText() {
+    this.paragraphText = 'Oh, You clicked something';
+  }
+
+}
